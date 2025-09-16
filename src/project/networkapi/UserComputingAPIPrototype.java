ac1db0c36d39e96c06a6e76b783dca4cf3bc89bb
Cesar09121 <letranquangtung1998@gmail.com>
2025-09-15 20:45:23 -0400

    Updating UserComputingAPIPrototype class

--- src/project/networkapi/UserComputingAPIPrototype.java
+++ src/project/networkapi/UserComputingAPIPrototype.java
@@ -8,26 +8,18 @@ import project.annotations.NetworkAPIPrototype;
  * Real implementation would actually process the computing job
  */
 
-public class UserComputingAPIPrototype implements UserComputingAPI {
+public class UserComputingAPIPrototype {
 		
-	@Override
 	@NetworkAPIPrototype
 	
-	public ComputingJobResponse submission(ComputingJobRequest computingJobRequest) {
+	public void prototype(UserComputingAPI userComputingAPI){
+		
+		// Call the submission method on the API
+		// Submit computing job with input source, output destination, and delimiters
+		
+		ComputingJobResponse response = userComputingAPI.submission(new ComputingJobRequest(null, null, null));
 		
 		
-		// Use default delimiters if none provided
-		Delimiters delimiters = computingJobRequest.getDelimitersOrDefault();
-		
-		
-		// Create successful response using DefaultComputingJobResponse implementation
-		// Maintains wire compatibility if Input/Output implementation changes
-		// Returns original input/output objects
-		return new DefaultComputingJobResponse(
-				computingJobRequest.getInput(),
-				computingJobRequest.getOutput(),
-				delimiters,
-				ComputingJobSuccess.SUCCESS);
 					
 	}
 
