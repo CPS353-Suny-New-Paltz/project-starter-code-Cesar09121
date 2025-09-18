195c66f09182f40c661dac8137ce8a7eed5529e4
Cesar09121 <letranquangtung1998@gmail.com>
2025-09-18 12:18:24 -0400

    Update DataWriteRequest.java

--- src/project/processapi/DataWriteRequest.java
+++ src/project/processapi/DataWriteRequest.java
@@ -1,19 +1,26 @@
 package project.processapi;
 
+/*
+ * Request for writing computation output to the storage location
+ * Contains the final destination and formatted result
+ */
 public class DataWriteRequest {
 	
 	private final String location;
 	private final String formattedResult;
 	
+	// Creates a new request for writing result
 	public DataWriteRequest(String location, String formattedResult) {
 		this.location=location;
 		this.formattedResult=formattedResult;
 	}
 
+	// Gets the output location for writing result
 	public String getLocation() {
 		return location;
 	}
 
+	// Returns computation result as a formatted string
 	public String getFormattedResult() {
 		return formattedResult;
 	}
