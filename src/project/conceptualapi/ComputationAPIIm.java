4ca6f0adf9c29c108c3be415e3a5b651922f4697
Cesar09121 <letranquangtung1998@gmail.com>
2025-09-26 21:46:18 -0400

    Adding Implement classes

---
+++ src/project/conceptualapi/ComputationAPIIm.java
@@ -0,0 +1,15 @@
+package project.conceptualapi;
+
+public class ComputationAPIIm implements ComputationAPI{
+
+	    @Override
+	    public long computeFactorial(int input) {
+	        return 0; // Default value
+	    }
+	    
+	    @Override
+	    public String formatResult(int input, String delimiters, long output) {
+	        return ""; // Empty string default
+	    }
+
+}
---
+++ src/project/processapi/DataStorageAPIIm.java
@@ -0,0 +1,5 @@
+package project.processapi;
+
+public class DataStorageAPIIm {
+
+}
