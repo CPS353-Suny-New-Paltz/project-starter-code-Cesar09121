1b36d0713da5468a81cb4578e20e576a2e546c37
Cesar09121 <letranquangtung1998@gmail.com>
2025-12-06 21:51:50 -0500

    Update the client based on professor's Checkpoint 7 feedback.
    The client now accepts user-provided in-memory input as well as file input
    Add temp file pattern to .gitignore

--- src/client/ComputeClient.java
+++ src/client/ComputeClient.java
@@ -6,6 +6,9 @@ import grpc.computation.ComputingJobRequest;
 import grpc.computation.ComputingJobResult;
 import grpc.computation.JobStatusCode;
 import grpc.computation.ComputingServiceGrpc;
+
+import java.io.File;
+import java.io.PrintWriter;
 import java.util.Scanner;
 /*
  * User-facing client for submitting factorial computation jobs
@@ -20,16 +23,31 @@ public class ComputeClient {
         String delimiter;
 
         if (args.length >= 2) {       	
-            inputLocation = args[0];
-            outputLocation = args[1];
-            delimiter = args.length >= 3 ? args[2] : ";";
+        	
+            if(args[0].equals("-number")) {
+            	String numbers = args[1];
+            	inputLocation = tempFile(numbers);
+                outputLocation = args[2];
+                delimiter = args.length >= 4 ? args[3] : ";";
+            } else {
+            	inputLocation = args[0];
+                outputLocation = args[1];
+                delimiter = args.length >= 3 ? args[2] : ";";
+            }
             
         } else {
+        	
             Scanner scanner = new Scanner(System.in);
 
-            System.out.print("Input file: ");
+            System.out.print("Enter number or input file: ");
+            String input = scanner.nextLine();
             
-            inputLocation = scanner.nextLine();
+            // Checks if input is a file or numbers
+            if (new File(input).exists()) {           	
+                inputLocation = input;  // It's a file              
+            } else {            	
+                inputLocation = tempFile(input);  // It's numbers
+            }
 
             System.out.print("Output file: ");
             
@@ -70,7 +88,32 @@ public class ComputeClient {
 
         // Closes the connection
         channel.shutdown();
-
-
+        
+		}
+        // Creates temporary file from comma-separated numbers
+        private static String tempFile(String numbers) {
+        	
+            try {
+            	// Generate unique filename with timestamp
+            	long timestamp = System.currentTimeMillis();
+            	String filename = "test/temp_input_" + timestamp +".txt";
+            	File temp = new File(filename);
+                
+                PrintWriter writer = new PrintWriter(temp);
+                for (String num : numbers.split(",")) {
+                    writer.println(num.trim());
+                }
+                
+                writer.close();
+                
+                return filename;
+                
+            } catch (Exception e) {
+            	
+                System.err.println("Error: " + e.getMessage());
+                System.exit(1);
+                return null;
+                
+            }
 	}
 }
