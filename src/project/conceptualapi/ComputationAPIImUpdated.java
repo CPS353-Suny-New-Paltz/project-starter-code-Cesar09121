92a4d4b504b132538d1a81628f6d63d8e30f4518
Cesar09121 <letranquangtung1998@gmail.com>
2025-12-08 19:19:31 -0500

    Add comments to explain the bottleneck and optimized implementation
    Add comments to explain the bottleneck and updated/optimized implementation

--- src/project/conceptualapi/ComputationAPIImUpdated.java
+++ src/project/conceptualapi/ComputationAPIImUpdated.java
@@ -1,5 +1,18 @@
 package project.conceptualapi;
-
+/*
+ * 1. Created a benchmark to test the run time of the original method
+ * 2. Noticed that the same factorial numbers were recalculated over again so many times that caused the bottlenecks
+ * 3. Added the updated factorial method using caching so the numbers wouldn't need to he recalculated each time
+ * 4. Updated benchmark to run both original and updated computation methods using the same inputs
+ * 5. Compared run times and calculated the performance improvement 
+ * 6. The results showed the minimum of 50% improvement with updated implementation
+ * 
+ * 
+ * Benchmark test compares the original and updated implementations:
+ * - Original: 6ms
+ * - Updated: 2ms
+ * - Improvement: 66.7% ( > 10%)
+ */
 public class ComputationAPIImUpdated implements ComputationAPI{
 	
 	private static final long [] cache = new long [21];
